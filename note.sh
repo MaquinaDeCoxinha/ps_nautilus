90c1b78921641b689aa2ce4b1758779d8792d815
MaquinaDeCoxinha <133978059+MaquinaDeCoxinha@users.noreply.github.com>
2023-07-27 12:21:09 -0300

    first add

---
+++ note.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> ~/notes
+echo -e "$(date) \n" >> ~/notes
+echo -e "$1" >> ~/notes
+echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" >> ~/notes
---
+++ welcome.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -e "$(date) \n"
+echo "Bem vindo $USER, voce esta no terminal do $HOSTNAME"
+curl wttr.in/riodejaneiro?0
+
